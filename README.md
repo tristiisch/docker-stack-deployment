13d0eea2afa733b55e5ef5cfdc9045ebc24884fa
Al-waleed Shihadeh <wshihadeh.dev@gmail.com>
2020-05-09 14:54:18 +0200

    Fork of https://github.com/wshihadeh/docker-deployment-action 87e582

---
+++ README.md
@@ -0,0 +1,84 @@
+# Docker Deployment Action
+
+A [GitHub Action](https://github.com/marketplace/actions/docker-deployment) that supports docker-compose and Docker Swarm deployments. [Documentaion Page](https://wshihadeh.github.io/actions/Docker-Deployment/).
+
+
+## Example
+
+Below is a brief example on how the action can be used:
+
+```yaml
+- name: Deploy to Docker swarm
+  uses: wshihadeh/docker-deployment-action@v1
+  with:
+    remote_docker_host: user@myswarm.com
+    ssh_private_key: ${{ secrets.DOCKER_SSH_PRIVATE_KEY }}
+    ssh_public_key: ${{ secrets.DOCKER_SSH_PUBLIC_KEY }}
+    deployment_mode: docker-swarm
+    copy_stack_file: true
+    deploy_path: /root/my-deployment
+    stack_file_name: docker-compose.yaml
+    keep_files: 5
+    args: my_applicaion
+```
+
+## Input Configurations
+
+Below are all of the supported inputs. Some inputs are considered sensitive information and it should be stored as secrets.
+
+### `args`
+
+Arguments to pass to the deployment command either  `docker`  or `docker-compose`. The actions will automatically generate the follwing commands for each of the cases.
+
+- `docker stack deploy --compose-file $FILE --log-level debug --host $HOST`
+- `docker-compose -f $INPUT_STACK_FILE_NAME`
+
+
+### `remote_docker_host`
+
+Specify Remote Docker host. The input value must be in the follwing format (user@host)
+
+### `remote_docker_port`
+
+Specify Remote Docker ssh port if its not 22 default ie (2222)
+
+### `ssh_public_key`
+
+Remote Docker SSH public key. 
+
+Do not give the content of `id_rsa.pub`
+
+The content of `~/.ssh/known_hosts` needs to be given here. You can get it by connecting to host once using your own machine. Example:
+
+```
+1.1.1.1 ecdsa-sha2-nistp256 AAAAE2VjZHNhLNTYAAAAIbmlzdHAyNCN5F3TLxUllpSRx8y+9C2uh+lWZDFmAsFMjcz2Zgq4d5F+oGicGaRk=
+```
+
+### `ssh_private_key`
+
+SSH private key used to connect to the docker host
+
+SSH key must be in PEM format (begins with -----BEGIN RSA PRIVATE KEY-----), or you can randomly get _Load key "/HOME/.ssh/id_rsa": invalid format_ error.
+
+Convert it from OPENSSH (key begins with -----BEGIN OPENSSH PRIVATE KEY-----)  format using `ssh-keygen -p -m PEM -f ~/.ssh/id_rsa`
+
+### `deployment_mode`
+Deployment mode either docker-swarm or docker-compose. Default is docker-compose.
+### `copy_stack_file`
+Copy stack file to remote server and deploy from the server. Default is false.
+### `deploy_path`
+The path where the stack files will be copied to. Default ~/docker-deployment.
+### `stack_file_name`
+Docker stack file used. Default is docker-compose.yaml
+### `keep_files`
+Number of the files to be kept on the server. Default is 3.
+### `docker_prune`
+A boolean input to trigger docker prune command.
+### `pre_deployment_command_args`
+The args for the pre deploument command. Applicable only for docker-compose.
+### `pull_images_first`
+Pull docker images before deploying. Applicable only for docker-compose.
+
+## License
+
+This project is licensed under the MIT license. See the [LICENSE](LICENSE) file for details.
---
+++ docker-entrypoint.sh
@@ -0,0 +1,116 @@
+#!/bin/sh
+set -eu
+
+execute_ssh(){
+  echo "Execute Over SSH: $@"
+  ssh -q -t -i "$HOME/.ssh/id_rsa" \
+      -o UserKnownHostsFile=/dev/null \
+      -p $INPUT_REMOTE_DOCKER_PORT \
+      -o StrictHostKeyChecking=no "$INPUT_REMOTE_DOCKER_HOST" "$@"
+}
+
+if [ -z "$INPUT_REMOTE_DOCKER_PORT" ]; then
+  INPUT_REMOTE_DOCKER_PORT=22
+fi
+
+if [ -z "$INPUT_REMOTE_DOCKER_HOST" ]; then
+    echo "Input remote_docker_host is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_SSH_PUBLIC_KEY" ]; then
+    echo "Input ssh_public_key is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_SSH_PRIVATE_KEY" ]; then
+    echo "Input ssh_private_key is required!"
+    exit 1
+fi
+
+if [ -z "$INPUT_ARGS" ]; then
+  echo "Input input_args is required!"
+  exit 1
+fi
+
+if [ -z "$INPUT_DEPLOY_PATH" ]; then
+  INPUT_DEPLOY_PATH=~/docker-deployment
+fi
+
+if [ -z "$INPUT_STACK_FILE_NAME" ]; then
+  INPUT_STACK_FILE_NAME=docker-compose.yaml
+fi
+
+if [ -z "$INPUT_KEEP_FILES" ]; then
+  INPUT_KEEP_FILES=4
+else
+  INPUT_KEEP_FILES=$((INPUT_KEEP_FILES+1))
+fi
+
+STACK_FILE=${INPUT_STACK_FILE_NAME}
+DEPLOYMENT_COMMAND_OPTIONS=""
+
+
+if [ "$INPUT_COPY_STACK_FILE" == "true" ]; then
+  STACK_FILE="$INPUT_DEPLOY_PATH/$STACK_FILE"
+else
+  DEPLOYMENT_COMMAND_OPTIONS=" --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT"
+fi
+
+case $INPUT_DEPLOYMENT_MODE in
+
+  docker-swarm)
+    DEPLOYMENT_COMMAND="docker $DEPLOYMENT_COMMAND_OPTIONS stack deploy --compose-file $STACK_FILE"
+  ;;
+
+  *)
+    INPUT_DEPLOYMENT_MODE="docker-compose"
+    DEPLOYMENT_COMMAND="docker-compose $DEPLOYMENT_COMMAND_OPTIONS -f $STACK_FILE"
+  ;;
+esac
+
+
+SSH_HOST=${INPUT_REMOTE_DOCKER_HOST#*@}
+
+echo "Registering SSH keys..."
+
+# register the private key with the agent.
+mkdir -p "$HOME/.ssh"
+printf '%s\n' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa"
+chmod 600 "$HOME/.ssh/id_rsa"
+eval $(ssh-agent)
+ssh-add "$HOME/.ssh/id_rsa"
+
+echo "Add known hosts"
+printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" > /etc/ssh/ssh_known_hosts
+
+if ! [ -z "$INPUT_DOCKER_PRUNE" ] && [ $INPUT_DOCKER_PRUNE = 'true' ] ; then
+  yes | docker --log-level debug --host "ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT" system prune -a 2>&1
+fi
+
+if ! [ -z "$INPUT_COPY_STACK_FILE" ] && [ $INPUT_COPY_STACK_FILE = 'true' ] ; then
+  execute_ssh "mkdir -p $INPUT_DEPLOY_PATH/stacks || true"
+  FILE_NAME="docker-stack-$(date +%Y%m%d%s).yaml"
+
+  scp -i "$HOME/.ssh/id_rsa" \
+      -o UserKnownHostsFile=/dev/null \
+      -o StrictHostKeyChecking=no \
+      -P $INPUT_REMOTE_DOCKER_PORT \
+      $INPUT_STACK_FILE_NAME "$INPUT_REMOTE_DOCKER_HOST:$INPUT_DEPLOY_PATH/stacks/$FILE_NAME"
+
+  execute_ssh "ln -nfs $INPUT_DEPLOY_PATH/stacks/$FILE_NAME $INPUT_DEPLOY_PATH/$INPUT_STACK_FILE_NAME"
+  execute_ssh "ls -t $INPUT_DEPLOY_PATH/stacks/docker-stack-* 2>/dev/null |  tail -n +$INPUT_KEEP_FILES | xargs rm --  2>/dev/null || true"
+
+  if ! [ -z "$INPUT_PULL_IMAGES_FIRST" ] && [ $INPUT_PULL_IMAGES_FIRST = 'true' ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
+    execute_ssh ${DEPLOYMENT_COMMAND} "pull"
+  fi
+
+  if ! [ -z "$INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" ] && [ $INPUT_DEPLOYMENT_MODE = 'docker-compose' ] ; then
+    execute_ssh "${DEPLOYMENT_COMMAND}  $INPUT_PRE_DEPLOYMENT_COMMAND_ARGS" 2>&1
+  fi
+
+  execute_ssh ${DEPLOYMENT_COMMAND} "$INPUT_ARGS" 2>&1
+else
+  echo "Connecting to $INPUT_REMOTE_DOCKER_HOST... Command: ${DEPLOYMENT_COMMAND} ${INPUT_ARGS}"
+  ${DEPLOYMENT_COMMAND} ${INPUT_ARGS} 2>&1
+fi
